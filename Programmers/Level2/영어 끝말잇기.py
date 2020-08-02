fb4709ab971f22a0857a90324ada1554e2dab911
YonghunPark <yw9142@gmail.com>
2020-08-02 22:28:56 +0900

    실패한 코드 추가

--- Programmers/Level2/영어 끝말잇기.py
+++ Programmers/Level2/영어 끝말잇기.py
@@ -9,3 +9,21 @@ def solution(n, words):
     return answer
 
 # 단어의 끝부분과 시작부분 확인 and 단어가 중복되는지 확인 둘중하나라도 걸린다면 answer 좌표 수정
+
+# 실패한 코드
+# def solution(n, words):
+#     answer = [0, 0]
+#     new_word = []
+#
+#     for i in range(1, len(words)):
+#         if words[i - 1][-1] != words[i][0]:
+#             answer = [(i % n) + 1, (i // n) + 1]
+#             break
+#
+#         if words[i - 1] not in new_word:
+#             new_word.append(words[i - 1])
+#         else:
+#             answer = [(i % n) + 1, (i // n) + 1]
+#             break
+#     return answer
+#
